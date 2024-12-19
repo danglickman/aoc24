d352d986d763f043084c9a3edc5f3173d51783f3
danglickman <dg4029@nyu.edu>
2024-12-19 02:37:15 -0500

    Day 19 p2

--- 19/aoc19.py
+++ 19/aoc19.py
@@ -2,29 +2,28 @@ import functools
 
 input = open('input', 'r').read().strip()
 
-towels, onsens= input.split('\n\n')
+towels, onsens = input.split('\n\n')
 
 towels = [t.strip() for t in towels.split(',')]
 onsens = onsens.splitlines()
 
 @functools.lru_cache(maxsize=None)
 def makable(o):
-    res = 0
     if len(o) == 0:
         return 1
+
+    res = 0
     for t in towels:
         if o.startswith(t):
-            if n :=makable(o[len(t):]):
-                res +=n
+            res += makable(o[len(t):])
     return res
 
 p1 = 0
 p2 = 0
 for o in onsens:
-    if n :=makable(o):
+    if n := makable(o):
         p1+=1
         p2 += n
-        # print(o)
 
 print(p1)
 print(p2)
