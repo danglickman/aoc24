af15f91b4beb7069f9b9e1bb6fa4dffa38dcb4f2
danglickman <dg4029@nyu.edu>
2024-12-23 01:27:54 -0500

    Day 23 (p2 cleaned up)

--- 23/aoc23p2.py
+++ 23/aoc23p2.py
@@ -1,16 +1,7 @@
 import networkx as nx
 
 input = open('input', 'r').read().strip().splitlines()
-edge_list = [l.split("-") for l in input]
-
-edges = set()
-nodes = set()
-for n1, n2 in edge_list:
-    nodes.add(n1)
-    nodes.add(n2)
-    edges.add((n1, n2))
-    edges.add((n2, n1))
-
+edges = [tuple(l.split("-")) for l in input]
 
 G = nx.Graph()
 G.add_edges_from(edges)
